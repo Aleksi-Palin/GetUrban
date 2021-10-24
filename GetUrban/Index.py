506ac1c23ce7be4cb134d3e75d8b6f70532d723a
Aleksi-Palin <69172196+Aleksi-Palin@users.noreply.github.com>
2021-10-24 04:28:59 +0300

    Maybe better version :/

--- GetUrban/Index.py
+++ GetUrban/Index.py
@@ -4,13 +4,82 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import time
 import os
+import warnings
+from colorama import Fore, init
 
+warnings.filterwarnings('ignore')
 
-#create function for getting urban word
-def GetUrban():
+def GetUrbanW1():
     #try getting urban word
-    try:
+    
+        #set chrome options
+        cOptions = Options()
+        cOptions.headless = True
 
+        #set chromedriver path
+        drive_path = os.path.abspath(os.curdir) + "\chromedriver.exe"
+        driver = webdriver.Chrome(executable_path= drive_path,chrome_options=cOptions)
+
+        #get wanted url
+        driver.get('https://urbaanisanakirja.com/random')     
+
+        #wait 1 seconds for whole page to load
+        time.sleep(1)    
+
+        #accept cookies
+        accept_div = driver.find_element(By.CSS_SELECTOR,'button.css-47sehv')
+        accept_div.click()
+        
+#get context-------------------------------------------------------------
+
+        #get the word
+        TheWord = driver.find_element(By.XPATH,"//div[@class='box']/a/h1").text
+
+        #wait 0.1 seconds for whole page to load
+        time.sleep(0.1)
+
+        #get the part that explains the word
+        explainer = driver.find_element(By.XPATH,"//div[@class='box']/p").text
+
+        example = driver.find_element(By.XPATH, "//div[@class='box']/blockquote").text
+
+        #wait 0.1 seconds for whole page to load
+        time.sleep(0.1)
+
+        #try to get blockquote element if not existing skip
+        init(convert=True)      
+        Widness = len(example) + 2 
+        print(f'{Fore.WHITE}_'*Widness)
+
+        #wait 0.1 seconds for whole page to load
+        time.sleep(0.1)
+
+        print(f'{Fore.GREEN} {TheWord}')
+                #print("-"*len(TheWord))
+        print(" ")
+
+        print(f'{Fore.MAGENTA} {explainer}')
+        print(" ")
+        print(f'{Fore.CYAN} {example}')
+
+        #wait 0.1 seconds for whole page to load
+        time.sleep(0.1)
+
+        print(f'{Fore.WHITE}_'*Widness)
+
+        #wait 0.1 seconds for whole page to load
+        time.sleep(0.1)
+                   
+#get context-------------------------------------------------------------
+            
+        
+        driver.quit()
+
+
+#create function for getting urban word without example
+def GetUrbanW2():
+    #try getting urban word
+    
         #set chrome options
         cOptions = Options()
         cOptions.headless = True
@@ -44,31 +113,37 @@ def GetUrban():
         time.sleep(0.1)
 
         #try to get blockquote element if not existing skip
-        try:
-            #get the example for the word
-            example = driver.find_element(By.XPATH,"//div[@class='box']/blockquote").text
-        except:
-            print("there is not blockquote element")
-            
-        finally:
-            
+        init(convert=True)      
+        Widness = len(explainer) + 2 
+        print(f'{Fore.WHITE}_'*Widness)
+
+        #wait 0.1 seconds for whole page to load
+        time.sleep(0.1)
+
+        print(f'{Fore.GREEN} {TheWord}')
+                #print("-"*len(TheWord))
+        print(" ")
+
+        print(f'{Fore.MAGENTA} {explainer}')
+
+        #wait 0.1 seconds for whole page to load
+        time.sleep(0.1)
+
+        print(f'{Fore.WHITE}_'*Widness)
+
+        #wait 0.1 seconds for whole page to load
+        time.sleep(0.1)
+                   
 #get context-------------------------------------------------------------
-
-            #sum up output
-            output = TheWord + "\n" + explainer + "\n" + example
-
-            #print output
-            print(output)
+            
         
-    except Exception as e:
-        #print error
-        print(e)
-    finally:
-        #when all succeed close browser
         driver.quit()
 
 #execute function in main
 if __name__ == "__main__":
-    GetUrban()
+    try:
+        GetUrbanW1()
+    except:
+        GetUrbanW2()
     
 
